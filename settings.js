3050a3216c19870eb62d078bc0c335155e4bc48f
MARCOMALIKking <sherrylaal2@gmail.com>
2026-05-12 22:05:07 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,22 @@
+const settings = {
+  prefixes: ['.', '!', '/', '#'], // Multiple prefix support you can add one or more
+  packname: 'MARCO MALIK & SYED-MD',
+  author: '‎GlobalTechInfo',
+  timeZone: 'Asia/Karachi',
+  botName: "MARCO MALIK & SYED-MD", // your bot name
+  botOwner: 'MARCO MALIK and SYED ABDUL WAHAB', // set your name
+  ownerNumber: '923706328012', // Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20,
+  tempCleanupInterval: 1 * 60 * 60 * 1000, // 1 hours
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "5.1.0",
+  updateZipUrl: "https://github.com/GlobalTechInfo/MEGA-MD/archive/refs/heads/main.zip",
+  channelLink: "https://whatsapp.com/channel/0029Vb2RnBlHgZWeOydeRV1H",
+  ytch: "GlobalTechInfo"
+};
+
+module.exports = settings;
+
